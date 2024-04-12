364627a8caac1f885c21ee0f0774ef0c6fc4acea
Murali22111997 <muralidharan.selvaraj997@outlook.com>
2024-04-12 22:11:21 +0530

    My project added Successfully

---
+++ src/a/MuraliGitClass.java
@@ -0,0 +1,21 @@
+package a;
+public class MuraliGitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //Hey This is Murali's Code 
+		
+		System.out.print("GIT Codes ");
+		/* $ git init
+		   $ git config --global user.name "Murali22111997"
+		   $ git config --global user.email "muralidharan.selvaraj997@outlook.com"
+		   $ git add. 
+		   $ git status
+           $ git commit -m "Murali Project Added Successfully"
+           $ git remote set-url origin git 
+           $ git push -u origin master
+		  
+		  */
+	}
+
+}
